545ae86616b6679c65507a42a8fe106e9a292be6
Jorge Henrique de Padua Silva <jorgehenriquepadua@gmail.com>
2021-05-16 14:36:01 +0200

    11 - Rotas com parâmetros no Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,33 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::get('/produtos/{idProduct?}', function($idProduct = ''){
+    return "Produto(s) {$idProduct}";
+});
+
+Route::get('/categoria/{flag}/posts', function ($flag){
+    return "Produtos da categoria: {$flag} posts";
+});
+
+Route::get('/categorias/{flag}', function ($flag){
+    return "Produtos da categoria: {$flag}";
+});
+
+Route::post('/register', function(){
+    return '';
+});
+
+Route::get('/empresa' ,function(){
+    return "Sobre a empresa";
+});
+
+
+Route::get('/contato' ,function(){
+    return view('site.contact');
+});
 
 Route::get('/', function () {
     return view('welcome');
